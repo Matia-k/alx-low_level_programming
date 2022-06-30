d1dc576e56d843cab19e2438ea4bef6825c8b8df
Matia-k <matia40k@gmail.com>
2022-06-30 16:15:16 -0700

    progrma prints sizes of different types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main-prints sizes of different types
+ * takes no argument
+ *
+ * Return: an int, zero when succesful
+ */
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)");
+	puts("Size of an int: 4 byte(s)");
+	puts("Size of a long int: 4 byte(s)");
+	puts("Size of a long long int: 8 byte(s)");
+	puts("Size of a float: 4 byte(s)");
+	return (0);
+}
