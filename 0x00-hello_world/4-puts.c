e07575668613868516f1042e60993ba10194fc28
Matia-k <matia40k@gmail.com>
2022-06-30 15:24:51 -0700

    program printis using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
-# !/ bin / bash
-#include stdio.h
-
+#!/ bin / bash
+#include<stdio.h>
+/**
+ * main - takes no argument
+ * Returns zero
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
