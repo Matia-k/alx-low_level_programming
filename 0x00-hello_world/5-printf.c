889a70bd1a59d0ecf99b1ad051b7d15b6e25d036
Matia-k <matia40k@gmail.com>
2022-06-30 15:59:41 -0700

    program prints string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - prints a string
+ * takes no argument
+ *
+ * Return: zero if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
