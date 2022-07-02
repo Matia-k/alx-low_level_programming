2a9f6149a13c5cdcda4e2eb2d6523f6eb519bafb
Matia-k <matia40k@gmail.com>
2022-07-01 18:40:07 -0700

    Prg prints single digits of base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Prints single digit numbers of base 10.
+  *
+  *Return: zero once succesful
+  */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
