a5f374037605f3d44a9882776bc47f7b3cc5acd1
Matia-k <matia40k@gmail.com>
2022-07-07 18:15:49 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   *print_most_numbers - prints numbers 0 - 9,followed by anew line
-  *
+  *Does not print 2 and 4.
   */
 void print_most_numbers(void)
 {
@@ -9,6 +9,8 @@ void print_most_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
+		if (i == 2 || i == 4)
+			continue;
 		putchar(i);
 	}
 	putchar('\n');
