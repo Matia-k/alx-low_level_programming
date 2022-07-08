a49e6cf703bd64ab33a06018903c8df7ae650ada
Matia-k <matia40k@gmail.com>
2022-07-07 17:35:22 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *mul - mutltiplies two integers
+  *@a: one of the integers
+  *@b: the other integer
+  *Return: the product of the two ints
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,10 +7,9 @@
 #include <stdio.h>
 
 
-
-void positive_or_negative(int i);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */
 
