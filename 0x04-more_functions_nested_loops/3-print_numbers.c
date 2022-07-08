1993003e607e92726a36703948e592634869b419
Matia-k <matia40k@gmail.com>
2022-07-07 17:50:43 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *print_most_numbers - prints numbers 0 - 9,followed by anew line
+  *
+  */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
