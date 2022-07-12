b8052ecad9dd99ad3bbee1a279c276f7939fc272
Matia-k <matia40k@gmail.com>
2022-07-12 11:55:05 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string.
+ * @str: the string to be printed.
+*/
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = (len / 2); i <= len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (len % 2 != 0)
+	{
+		len++;
+		for (i = ((len / 2) + 1); i <= len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
