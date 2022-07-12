91134f5baa3429427b267529a0981b4c5833d541
Matia-k <matia40k@gmail.com>
2022-07-12 07:46:25 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *rev_string - reverses a string.
+  *@s: string to be reversed.
+*/
+void rev_string(char *s)
+{
+	int len, i;
+	char c;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[len - i];
+		s[len - i] = c;
+	}
+}
