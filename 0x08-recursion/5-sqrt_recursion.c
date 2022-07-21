31b6752b329d4b877775ed29cb04baa2fb4d3e03
Matia-k <matia40k@gmail.com>
2022-07-21 03:44:28 -0700

    completed task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int rep_subtraction(int number, int count);
 /**
 *_sqrt_recursion - returns the natural square root of a number.
 *@n: The number whose square root is to be found.
@@ -7,10 +8,32 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	int ret, odd = 1;
+
+	if (n <= 0)
 		return (-1);
-	else if (n == 0)
+
+	ret = rep_subtraction(n, odd);
+	if (ret < 0)
+		return (-1);
+	
+	return (ret);
+}
+
+/**
+*rep_subtraction - subtracts the consecutive odd numbers from a number,
+*until it's zero. "repeated subtraction method".
+*@number: the number to be subtracted.
+*@count: keeps track of the number of subtractions.
+*Return: the number of subtractions.
+*/
+int rep_subtraction(int number, int count)
+{
+	if (number == 0)
 		return (0);
-	else if (n == 1)
-		return (1);
+	if (number < 0)
+		return (-1000);
+	number = number - count;
+	count = count + 2;
+	return (1 + rep_subtraction(number, count));
 }
