0792ddaf85b256e22e9070482c7b2664a42bdffc
Matia-k <matia40k@gmail.com>
2022-07-28 13:00:56 -0700

    completed task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -25,7 +25,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	type_arr = arr;
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < bytes; i++)
 		type_arr[i] = '\0';
 
 	return (arr);
