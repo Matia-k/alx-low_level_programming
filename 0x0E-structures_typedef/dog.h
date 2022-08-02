32fdd7efdd6b4a740a14083e247d6c5e59be40e2
Matia-k <matia40k@gmail.com>
2022-08-02 12:54:05 -0700

    added betty styling to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
 #ifndef DOG_H
-#define MAIN_H
+#define DOG_H
 
 /**
  *struct dog - a type describing a dog.
  *@name: name of the dog.
- *age: age of the dog.
- *owner: the owner of the dog.
+ *@age: age of the dog.
+ *@owner: the owner of the dog.
 */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};			
+};
 
 #endif
