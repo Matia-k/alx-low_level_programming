875a40f247c46716d533b8771cb04b89440e804a
Matia-k <matia40k@gmail.com>
2022-09-13 06:05:25 -0700

    completed task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_atoi - converts a string to an integer.
+*@s: string to be converted
+*Return: integer resulting from the conversion.
+*/
+int _atoi(char *s)
+{
+	int num = 0, i = 0, sign = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+			num = num * 10 + s[i] - '0';
+		if (s[i] >= '0' && s[i] <= '9' && s[i + 1] == ' ')
+			break;
+		i++;
+	}
+	return (num * sign);
+}
