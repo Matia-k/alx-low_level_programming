09013afa375b2ce84191729854cf4501f9b4536f
Matia-k <matia40k@gmail.com>
2023-01-28 10:03:47 -0800

    completed task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - Adds a new node at the beginning of a list.
+ * @head: A pointer to the head of the list
+ * @str: The string to add to the new node.
+ *
+ * Return: the address of the new element, or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *new_string;
+	unsigned int len;
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_string = strdup(str);
+	if (new_string == NULL)
+	{
+		free(new_string);
+		return (NULL);
+	}
+
+	len = strlen(new_string);
+
+	new_node->str = new_string;
+	new_node->len = len;
+	new_node->next = *head;
+
+	*head = new_node;
+	return (*head);
+}
