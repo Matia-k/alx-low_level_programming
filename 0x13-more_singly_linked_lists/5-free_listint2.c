125f2023d245a882eaa24d3697b294addf830150
Matia-k <matia40k@gmail.com>
2023-01-31 08:33:15 -0800

    completed task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to the beginning of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *buf;
+	/**
+	 * iterate through the list.
+	 * First keep track of address of next node(in a buffer pointer)
+	 * Then free current node pointer.
+	 * continue until end of list.
+	 */
+	while (*head)
+	{
+		buf = (*head)->next;
+		free(*head);
+		*head = buf;
+	}
+	head = NULL;
+}
