cfda157f1cc617992a236f9e618543c6748d18a4
Matia-k <matia40k@gmail.com>
2023-04-13 10:01:55 -0700

    corrected segmentation fault in handling more collisions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				ht->array[i] = node;
 			else
 			{
-				temp = ht->array[i]->next;
+				temp = ht->array[i];
 				while (temp)
 					temp = temp->next;
 				temp = node;
