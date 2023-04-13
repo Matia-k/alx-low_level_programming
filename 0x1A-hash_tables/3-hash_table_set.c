2e961d2b7d83ea8fb27f01ef96869c8d18510c52
Matia-k <matia40k@gmail.com>
2023-04-13 02:08:49 -0700

    fixed segmentation fault in handling collisions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,7 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			{
 				while (ht->array[i])
 					ht->array[i] = ht->array[i]->next;
-				ht->array[i]->next = node;
+				ht->array[i] = node;
 			}
 		}
 	}
