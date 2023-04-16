8eba3550abf52bd84c7f1d7dc005aa631d28c0cb
Matia-k <matia40k@gmail.com>
2023-04-16 03:18:43 -0700

    corrected typo on key variable

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	
-	if (ht == NULL || Key == NULL)
+	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
