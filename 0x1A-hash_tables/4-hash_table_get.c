a9705c2974c7888b8a89803d93b0775384a38f5c
Matia-k <matia40k@gmail.com>
2023-04-16 03:43:42 -0700

    took collisions into consideration

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,11 +9,16 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	
+
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
+
+	while (ht-array[index])
+	{
+		ht-array[index] = ht-array[index]->next;
+	}
 	return (ht->array[index]->value);
 }
